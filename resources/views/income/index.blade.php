7bc4b23eb1182d272ac0678d5730b413a5178870
Tu Nombre <you@example.com>
2025-01-23 14:42:09 +0100

    Creación del componente button y uso de la bolsa de

--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -2,5 +2,5 @@
 <x-layouts.index :title="$title">
   <x-table :tableData="$tableData" />
   <br>
-  <x-button href="mylink">Mi botón</x-button>
+  <x-button href="mylink" miclase="miclase1" class="btn_especial">Mi botón</x-button>
 </x-layouts.index>
